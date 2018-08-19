b51ad509be50decf763bd4a74c2d39be92b8955a
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 08:10:29 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,13 +4,16 @@ end
 def subtraction(num1, num2)
 end
 
+
 def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+   2 * 3 = 
 end
 
 def modulo(num1, num2)
+  7 %  5  = 2.
 end
 
 def square_root(num)
