42f1ad839f097bb4fe4dda41c1b50c8ae94079fc
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 08:16:26 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,28 @@
 def addition(num1, num2)
-  5+ 4
+  5 + 4
 end
 
 def subtraction(num1, num2)
-  9 - 5 
+  10- 5 
 end
 
 
 def division(num1, num2)
-  9.0/ 2 
+  50 / 2
 end
 
 def multiplication(num1, num2)
-   2 * 3
+   4 * 30
 end
 
 def modulo(num1, num2)
-  7 %  5  
+ 34 %  5  
 end
 
 def square_root(num)
-  Math.sqrt(3)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-    1 + (( 4 * 6 ) / 2 )
+    7 + (( 43 * 23 ) / 83 )
 end
